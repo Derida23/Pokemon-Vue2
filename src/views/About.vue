398f6701e355663ca00453c93825e05f1a7dcb3d
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2023-01-24 21:16:52 +0700

    feat: code about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>About</h1>
+  <div class="about">
+    <h1>About</h1>
+    <p>Hola, this is progress create pokedex with vuejs 2 and vuex</p>
+    <p>Trying something basic to learn fundamental vuejs</p>
+    <ul>
+      <li>Vue.js</li>
+      <li>Vuex</li>
+      <li>Vue-Router</li>
+      <li>PokeApi</li>
+      <li>Scss</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -7,3 +18,9 @@ export default {
   name: "AboutView",
 };
 </script>
+
+<style scoped lang="scss">
+.about {
+  padding: 0 15px;
+}
+</style>
